c33fdba1e93ae338138cd3e10f8d64a1ddfa43f2
Coinage@1790 <pradnyamandekar129@gmail.com>
2026-06-17 14:15:27 +0530

    baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://103.224.246.190:5010",
+  baseURL: "https://jobportal.coinage.in",
 //  baseURL:"https://localhost:7011",
   headers: {
     "Content-Type": "application/json",
